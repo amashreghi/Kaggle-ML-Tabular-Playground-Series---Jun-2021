24ef82130464e14ce653e34fc0bdc028b2653f55
Ali Mashreghi <ali.mashreghi87@gmail.com>
2021-07-30 15:45:53 -0700

    Adding some plots

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ import csv
 
 import pandas as pd
 from numpy import mean, std
+from sklearn import preprocessing
 from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+from matplotlib.ticker import StrMethodFormatter
 from sklearn.model_selection import RepeatedStratifiedKFold, cross_val_score
 
 training_set = pd.read_csv('../tabular-playground-series-jun-2021/train.csv')
@@ -15,17 +18,28 @@ test_set = pd.read_csv('../tabular-playground-series-jun-2021/test.csv')
 training_set.drop(columns=['id'], axis=1, inplace=True)
 test_set.drop(columns=['id'], axis=1, inplace=True)
 
+
+# plotting
+df = pd.DataFrame(training_set)
+ax = df.plot.hist(bins=12, alpha=0.5)
+plt.show()
+
+exit()
+
 x_train = training_set.iloc[:,training_set.columns != 'target'].values
 y_train = training_set.iloc[:,training_set.columns == 'target'].values.ravel()
 
 x_test = test_set
 
+
+
 #pd.set_option('display.max_columns',20)
 #pd.set_option('display.width', 1000)
 #print (training_set.head())
-
+scaler = preprocessing.StandardScaler().fit(x_train)
+x_train_scaled = scaler.transform(x_train)
 model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1)
-model.fit(x_train, y_train)
+model.fit(x_train_scaled, y_train)
 
 # define the model evaluation procedure
 #cv = RepeatedStratifiedKFold(n_splits=2, n_repeats=1, random_state=1)
@@ -36,7 +50,9 @@ model.fit(x_train, y_train)
 # report the model performance
 #print('Mean Accuracy: %.3f (%.3f)' % (mean(n_scores), std(n_scores)))
 
-test_y_classes = model.predict_proba(x_test)
+scaler = preprocessing.StandardScaler().fit(x_test)
+x_test_scaled = scaler.transform(x_test)
+test_y_classes = model.predict_proba(x_test_scaled)
 
 print(test_y_classes[0])
 
